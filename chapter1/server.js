237607dab077f5a603e815c1b3177019f2432fdd
inspirezuza <inspire7220@gmail.com>
2023-11-09 06:26:38 +0700

    update chapter1

--- chapter1/server.js
+++ chapter1/server.js
@@ -1,6 +1,20 @@
 const path = require("path");
 const os = require("os");
+const { add, subtract, multiply, divide } = require("./math");
 
-console.log(os.homedir());
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(multiply(2, 3));
+console.log(divide(2, 3));
 
-console.log(path.parse(__filename));
+// console.log(os.homedir());
+// console.log(os.type());
+// console.log(os.version());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(path.dirname(__filename));
+// console.log(path.extname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.parse(__filename));
